c3a9ea5b5ad4d1d585a3c512fa52b3376e3827b6
Focus4Study <f4studies@gmail.com>
2024-07-18 01:11:20 +0600

    server crash issue online fix

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
